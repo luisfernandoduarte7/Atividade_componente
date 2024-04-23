10f76f5bd2c371a5b0916ffa1e9f5bf87f428788
luisfernandogaldino <133809489+luisfernandogaldino@users.noreply.github.com>
2024-04-23 16:15:16 -0300

    Atividade_componente

---
+++ src/app/index.js
@@ -0,0 +1,25 @@
+import { View } from 'react-native';
+import { styles } from '../css/style';
+import Botao from '../components/botao';
+import InputsTexto from '../components/inputsTexto';
+import { useState } from 'react';
+
+export default function App() {
+  const [valida, setValida] = useState();
+  const atualizaTexto = (newText) => {
+    console.log(newText);
+  };
+
+  return (
+    <View style={styles.container}>
+      <InputsTexto label='Digite seu nome' dica="Digite seu nome" />
+      <InputsTexto valor={atualizaTexto} label='Digite seu CPF' dica="Digite seu CPF" />
+      <InputsTexto label='Digite sua senha' dica="Digite sua senha" seguranca={true} />
+
+      <Botao btn="entrar" cor='#000' src='home' />
+      <Botao btn="excluir" cor='#f00' src='excluido' />
+      <Botao btn="cadastrar" cor='#0f0' src='cadastrar' />
+    </View>
+  );
+}
+
